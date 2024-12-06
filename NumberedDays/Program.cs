30c62169bed4fbbd221e314c8c47e35c36dbad3c
Jakub Muzyczuk <jtmuzyczuk@hotmail.com>
2024-12-06 08:06:14 +0100

    It's a simple program to print day of the week based on user input.

---
+++ NumberedDays/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter the number corresponding to the day of the week (1-7): ");
+int numberedDay = int.Parse(Console.ReadLine());
+
+string namedDay;
+
+switch (numberedDay)
+{
+    case 1:
+        namedDay = "Monday";
+        break;
+    case 2:
+        namedDay = "Tuesday";
+        break;
+    case 3:
+        namedDay = "Wednesday";
+        break;
+    case 4:
+        namedDay = "Thursday";
+        break;
+    case 5:
+        namedDay = "Friday";
+        break;
+    case 6:
+        namedDay = "Saturday";
+        break;
+    case 7:
+        namedDay = "Sunday";
+        break;
+    default:
+        throw new Exception("Invalid day number.");
+}
+
+Console.WriteLine($"Day of the week: {namedDay}");
